54d0d463d76c44b60652b3c04791860b783a0d07
João Diego <jdflorencio@gmail.com>
2019-08-17 20:48:14 -0300

    start  server

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+
+const server = express()
+
+mongoose.connect('mongodb+srv://<user>:<passworld>@cluster0-fnus2.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true
+})
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
+
+server.listen(3333);
