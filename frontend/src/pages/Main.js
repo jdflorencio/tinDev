c8c8913b888364ace9b6d40f0da8e5df2ddc2ef6
João Diego <jdflorencio@gmail.com>
2019-08-20 05:02:14 -0300

    first push

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,60 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState }from 'react'
+import { Link } from 'react-router-dom'
+import api from '../services/api'
 import './Main.css'
 import Logo from '../assets/logo.svg'
 
-
 function Main({ match }) {
-    return(
-    
-    <div className="main-container">
-        <img src={ Logo } alt="tindev" />
-        <ul>
-            <li>
-                <img src="https://avatars1.githubusercontent.com/u/1665576?v=4" alt="celso" />
-                <footer>
-                    <strong> Programador </strong>
-                    <p>Programador ....</p>
+	const [users, setUsers] = useState([])
+	// dependencias dos useEffect
+	useEffect(() =>{
+	async function loadUsers() {
+		const response = await api.get('/devs', {
+			headers: { 
+					user: match.params.id,
+				}
+		})
+		setUsers(response.data)
+	}
 
-                </footer>
-                <div className="buttons">
-                    <button type="button">like
-                    </button>
-                    <button type="button">Dislike
-                    </button>
-                </div>
-                
-            </li>
-            <li>
-                <img src="https://avatars1.githubusercontent.com/u/1665576?v=4" alt="celso" />
-                <footer>
-                    <strong> Programador </strong>
-                    <p>Programador ....</p>
+	loadUsers()
+	}, [match.params.id])
 
-                </footer>
-                <div className="buttons">
-                    <button type="button">like
-                    </button>
-                    <button type="button">Dislike
-                    </button>
-                </div>
-            </li>
-            <li>
-                <img src="https://avatars1.githubusercontent.com/u/1665576?v=4" alt="celso" />
-                <footer>
-                    <strong> Programador </strong>
-                    <p>Programador ....</p>
+	async function handleLike(id) {
+		await api.post(`/devs/${id}/likes`, null, {
+			headers: {user: match.params.id}, 
+		})
+	
+		setUsers(users.filter(user => user._id != id))
+	}
 
-                </footer>
-                <div className="buttons">
-                    <button type="button">like
-                    </button>
-                    <button type="button">Dislike
-                    </button>
-                </div>
-            </li>
-        </ul>
-     </div>
-    )
+	async function handleDislike(id) {
+		await api.post(`/devs/${id}/dislike`, null, {
+			headers: {user: match.params.id}, 
+		})
+
+		setUsers(users.filter(user => user._id != id))
+	}
+	return(
+		<div className="main-container">
+			<Link to="/">
+				<img src={ Logo } alt="tindev"/>
+			</Link>
+			{users.length > 0 ? (
+				<ul>
+				{users.map( user => (
+				<li key={user._id}>
+					<img src={user.avatar} alt={user.name} />
+					<footer>
+						<strong> {user.name} </strong>
+						<p>{user.bio}</p>
+					</footer>
+					<div className="buttons">
+						<button type="button" onClick={() => handleLike(user._id)}>like
+						</button>
+						<button type="button"  onClick={() => handleDislike(user._id)}>Dislike
+						</button>
+					</div>			
+				</li>
+		))}	
+		</ul>
+			) : <div className="empty">Acabou :( </div>}	
+		</div>
+	)
 }
-
 export default Main
